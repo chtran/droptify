73ac48b5cd21567fdbea1a595d64c53c030b9eeb
Chau Tran <chau_tran@brown.edu>
2013-02-02 22:06:41 -0500

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Droptify::Application.config.secret_token = '561ca70fe18c976b4dcb1fd0ff44404171535b12efc5aea0659ee93b7cb42376af6b24e6e42606810913d452a50a676e92615a7ed2531b36fb39575793cb6fe6'
